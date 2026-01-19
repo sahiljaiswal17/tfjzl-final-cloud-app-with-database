b1c60698fc043d3501e7e574d14115191dee6abe
sahiljaiswal17 <sahiljaiswal17@outlook.com>
2026-01-19 15:20:05 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,8 +1,20 @@
-
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('course/<int:course_id>/submit/', views.submit_exam, name='submit_exam'),
-    path('course/<int:course_id>/result/', views.show_exam_result, name='show_exam_result'),
+    path(
+        'course/<int:course_id>/',
+        views.course_details,
+        name='course_details'
+    ),
+    path(
+        'course/<int:course_id>/submit/',
+        views.submit_exam,
+        name='submit_exam'
+    ),
+    path(
+        'course/<int:course_id>/result/<int:submission_id>/',
+        views.show_exam_result,
+        name='show_exam_result'
+    ),
 ]
