b98ee68a3eeaa9d49e16900bda8df9af86926b3e
sahiljaiswal17 <sahiljaiswal17@outlook.com>
2026-01-19 14:22:46 +0530

    Create models.py

---
+++ models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Question(models.Model):
+    course = models.ForeignKey('Course', on_delete=models.CASCADE)
+    question_text = models.CharField(max_length=200)
+    grade = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.question_text
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.choice_text
+
+
+class Submission(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    course = models.ForeignKey('Course', on_delete=models.CASCADE)
+    choices = models.ManyToManyField(Choice)
+    score = models.IntegerField(default=0)
